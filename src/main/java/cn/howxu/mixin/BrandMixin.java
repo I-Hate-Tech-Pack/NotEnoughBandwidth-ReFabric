f0d21525a748ac423c2d85a916be2723e758c200
HowXu <howxu366@outlook.com>
2026-04-13 20:15:52 +0800

    ✨ feat(BrandMixin):
    add Brand info

---
+++ src/main/java/cn/howxu/mixin/BrandMixin.java
@@ -0,0 +1,30 @@
+package cn.howxu.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.MathHelper;
+
+@Mixin(TitleScreen.class)
+public abstract class BrandMixin extends Screen {
+
+    protected BrandMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "render", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void onRender(DrawContext context, int mouseX, int mouseY, float deltaTicks, CallbackInfo ci, float f) {
+        int alpha = MathHelper.ceil(f * 255.0F);
+        String brand_info = "Performance enhanced for " + SharedConstants.getGameVersion().getName() + " by howxu";
+        context.drawTextWithShadow(this.textRenderer, brand_info, 2, this.height - 20, ColorHelper.getArgb(alpha,255,1,1));
+    }
+}
